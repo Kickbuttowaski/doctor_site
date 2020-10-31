2b24a69d59efa83453050a334d50cde6fb5ae6bd
Kickbuttowaski <promods96@gmail.com>
2020-10-31 11:46:15 +0530

    route fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,13 +38,13 @@ const Header = () => {
             <div style={{ width: "200px" }}></div>
             <ul>
               <li>
-                <a href="/doctor_site/home#home">Home</a>
+                <a href="/home#home">Home</a>
               </li>
               <li>
-                <a href="/doctor_site/home#facility">Facilities</a>
+                <a href="/home#facility">Facilities</a>
               </li>
               <li>
-                <a href="/doctor_site/home#about">About us</a>
+                <a href="/home#about">About us</a>
               </li>
             </ul>
           </nav>
